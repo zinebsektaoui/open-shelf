31f17e59469a577a4d869b0d5f87b20015d5ae65
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-07-18 21:23:08 +0100

    formulaire d'ajout

--- app/books/create/page.tsx
+++ app/books/create/page.tsx
@@ -1,12 +1,26 @@
-import Header from '@/layouts/Header'
-import React from 'react'
+"use client"
+import BookForm from "@/components/BookForm";
+import Footer from "@/layouts/Footer";
+import Header from "@/layouts/Header";
 
 function page() {
   return (
     <div>
-        <Header />
+      <Header />
+      <BookForm
+        onSubmit={async (data) => {
+          await fetch("/api/books", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          });
+        }}
+      />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
